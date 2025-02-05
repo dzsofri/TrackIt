1cafdea825a9eca3e167c465b443711ec7669639
dudaslevente <dudaslevente@turr.hu>
2025-02-05 19:57:46 +0100

    Profil oldal elkezdése

--- TrackIt/src/app/app.component.ts
+++ TrackIt/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { WelcomeComponent } from '../components/welcome/welcome.component';
 import { CommonModule } from '@angular/common';
 import { RegistrationComponent } from '../components/registration/registration.component';
+import { ProfileComponent } from '../components/profile/profile.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, WelcomeComponent, CommonModule, RegistrationComponent],
+  imports: [RouterOutlet, WelcomeComponent, CommonModule, RegistrationComponent, ProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
