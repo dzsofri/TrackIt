2e310c73419f4fb5bc91a85ff0c558b0a043f013
somodikonrad <somodikonrad@turr.hu>
2025-02-12 21:39:48 +0100

    Oldal váltás animáció elkezdése

---
+++ TrackIt/src/components/kanban/kanban.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-kanban',
+  imports: [CommonModule],
+  templateUrl: './kanban.component.html',
+  styleUrl: './kanban.component.scss'
+})
+export class KanbanComponent {
+
+}
