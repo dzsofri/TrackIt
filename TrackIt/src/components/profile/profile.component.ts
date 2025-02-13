62eb9a96b08dffd24107db5b42f0894484100176
dudaslevente <dudaslevente@turr.hu>
2025-02-13 10:28:07 +0100

    Tabok közötti váltás sikeres

--- TrackIt/src/components/profile/profile.component.ts
+++ TrackIt/src/components/profile/profile.component.ts
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
+  activeTab: string = 'statisztika';
 
+  setActiveTab(tabName: string) {
+    this.activeTab = tabName;
+  }
 }
