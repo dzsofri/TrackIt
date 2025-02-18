cd8f2b588f1d09820c0d7d1929be0a6e49e799c6
Beréti Zsófia <beretizsofia310@gmail.com>
2025-02-18 12:26:40 +0100

    sidebar pozícionálás

--- TrackIt/src/app/app.component.ts
+++ TrackIt/src/app/app.component.ts
@@ -3,13 +3,20 @@ import { RouterOutlet } from '@angular/router';
 import { WelcomeComponent } from '../components/welcome/welcome.component';
 import { CommonModule } from '@angular/common';
 import { RegistrationComponent } from '../components/registration/registration.component';
+import { SideBarComponent } from '../components/side-bar/side-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, WelcomeComponent, CommonModule, RegistrationComponent],
+  imports: [RouterOutlet, CommonModule, SideBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'TrackIt';
+
+  isSidebarOpen = false; // Kezdetben zárt
+
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
 }
