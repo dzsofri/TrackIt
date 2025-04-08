527dd6711a57ecfea96ac2d36b1677c96f46f0cd
somodikonrad <somodikonrad@turr.hu>
2025-04-08 12:17:43 +0200

    Poszt feltöltés kép nélkül

--- TrackIt/src/app/components/feed/feed.component.ts
+++ TrackIt/src/app/components/feed/feed.component.ts
@@ -1,29 +1,30 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { ProfileBejegyzesComponent } from '../profile-bejegyzes/profile-bejegyzes.component';
 import { NewpostComponent } from '../newpost/newpost.component';
 import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-feed',
   standalone: true,
-  imports: [CommonModule, FormsModule, ProfileBejegyzesComponent, NewpostComponent],
+  imports: [CommonModule, FormsModule, NewpostComponent],
   templateUrl: './feed.component.html',
   styleUrl: './feed.component.scss'
 })
 export class FeedComponent {
-  posts: any[] = [];
+  posts: any[] = []; // A posztok tárolása
 
   constructor(private apiService: ApiService) {}
 
   ngOnInit() {
-    this.loadPosts();
+    this.loadPosts(); // A posztok betöltése
   }
 
   loadPosts() {
     this.apiService.getPosts().subscribe(response => {
       this.posts = response.posts || [];
+    }, error => {
+      console.error('Hiba a posztok betöltésekor:', error);
     });
   }
 }
